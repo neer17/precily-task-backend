33671e11a2298d4b332cafebf310645c636b63ad
neer17 <neerajsewanisrt8@gmail.com>
2020-11-19 20:55:52 +0530

    feat: /save logic added.

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,11 @@ const log = console.log
 
 //  connecting to mLab
 console.info(`${process.env.MONGODB_URI}`)
-mongoose.connect(`${process.env.MONGODB_URI}`, { useNewUrlParser: true })
+mongoose.connect(`${process.env.MONGODB_URI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  retryWrites: false,
+})
 
 router.get('/', (req, res) => {
   res.send('Home Route')
@@ -17,32 +21,48 @@ router.get('/', (req, res) => {
 
 router.post('/save', async (req, res) => {
   const { key, content } = req.body
-  console.log(`key: ${key} \n content: ${content}`)
   try {
-    const file = await FileModel.find({
+    const file = await FileModel.findOne({
       key,
     }).exec()
-    log('File: ', file)
+    // log('File: ', file)
 
-    let fileModel
+    let count
     if (file) {
-      let { count } = file
-      count++
-      fileModel = new FileModel({
-        ...file,
+      count = ++file.count
+      const startTime = Date.now()
+      const queryResult = await FileModel.updateOne(
+        {
+          key,
+        },
+        {
+          content,
+          count,
+        }
+      )
+      log('document modified: ', queryResult.n)
+      const executionTime = queryResult.opTime.ts.high_ * 1000 - startTime
+      res.status(200).json({
         count,
+        executionTime,
       })
     } else {
-      fileModel = new FileModel({
-        ...file,
+      count = 1
+      const fileModel = new FileModel({
+        key,
+        content,
+        count,
+      })
+
+      const fileSaved = await fileModel.save()
+      console.log('fileSaved: ', fileSaved)
+      res.status(200).json({
         count,
       })
     }
-
-    const isFileSaved = await fileModel.save()
-    console.log('isFileSaved: ', isFileSaved)
   } catch (err) {
     console.error(err)
+    res.status(500).send(err)
   }
 })
 
